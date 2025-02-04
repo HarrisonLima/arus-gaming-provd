dbb11d79156f1746e46a16d6dcfb95afbccbea5b
Harrison Fabiano de Lima <harrisonlima92@gmail.com>
2025-02-04 20:15:58 -0300

    changed default lang

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -20,8 +20,8 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: 'en', // default language
-    fallbackLng: 'en',
+    lng: 'pt', // default language
+    fallbackLng: 'pt',
     interpolation: {
       escapeValue: false,
     },
